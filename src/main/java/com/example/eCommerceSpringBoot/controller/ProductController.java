26f34c92b5d9501515ab561454762fe29eb80c1f
Eduardo Arellano <josearellano97@hotmail.com>
2023-07-24 23:09:08 -0600

    -Se implementa en la vista el registro de la información ingresada con el método Post

--- src/main/java/com/example/eCommerceSpringBoot/controller/ProductController.java
+++ src/main/java/com/example/eCommerceSpringBoot/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.example.eCommerceSpringBoot.service.ProductService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,8 @@ public class ProductController {
     public Logger logger = LoggerFactory.getLogger(ProductController.class);
 
     @GetMapping("")
-    public String show(){
+    public String show(Model model){
+        model.addAttribute("products",productService.findAll());
         return "show";
     }
 
