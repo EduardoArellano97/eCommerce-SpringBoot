d77168ef1964c8253fd5bd6ef38ae575fb20b685
Eduardo Arellano <josearellano97@hotmail.com>
2023-07-26 15:00:42 -0600

    -Se habilitó la vista de las imagenes correspondientes a los productos dados de alta en el sistema, así como el cargar los archivos tipo imagen para describir cada producto

--- src/main/java/com/example/eCommerceSpringBoot/controller/AdminController.java
+++ src/main/java/com/example/eCommerceSpringBoot/controller/AdminController.java
@@ -1,15 +1,25 @@
 package com.example.eCommerceSpringBoot.controller;
 
+import com.example.eCommerceSpringBoot.model.Product;
+import com.example.eCommerceSpringBoot.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
+    @Autowired
+    private ProductService productService;
     @GetMapping("")
-    public String home(){
+    public String home(Model model){
+        List<Product> products = productService.findAll();
+        model.addAttribute("products",products);
+
         return "home";
     }
 
--- src/main/java/com/example/eCommerceSpringBoot/controller/ProductController.java
+++ src/main/java/com/example/eCommerceSpringBoot/controller/ProductController.java
@@ -3,15 +3,15 @@ package com.example.eCommerceSpringBoot.controller;
 import com.example.eCommerceSpringBoot.model.Product;
 import com.example.eCommerceSpringBoot.model.User;
 import com.example.eCommerceSpringBoot.service.ProductService;
+import com.example.eCommerceSpringBoot.service.UploadFileService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 
@@ -20,6 +20,8 @@ import java.util.Optional;
 public class ProductController {
     @Autowired
     private ProductService productService;
+    @Autowired
+    private UploadFileService uploadFileService;
     public Logger logger = LoggerFactory.getLogger(ProductController.class);
 
     @GetMapping("")
@@ -33,10 +35,15 @@ public class ProductController {
         return "create";
     }
     @PostMapping("/save")
-    public String save(Product product){
+    public String save(Product product, @RequestParam("img") MultipartFile file) throws IOException {
         logger.info("Objeto producto a continuación: {}",product);
         User user = new User(1L,"","","","","","","");
         product.setUser(user);
+        //implementación de image
+        if(product.getId()==null){ //Se valida que el producto sea nuevo para añadir nueva imagen.
+            String imageName= uploadFileService.saveImage(file);
+            product.setImage(imageName);
+        }
         productService.save(product);
         return "redirect:/products";
     }
@@ -50,12 +57,31 @@ public class ProductController {
         return "edit";
     }
     @PostMapping("/update")
-    public String update(Product product){
+    public String update(Product product, @RequestParam("img") MultipartFile file) throws IOException{
+        Product product1 = new Product();
+        product1=productService.get(product.getId()).get();
+        if(file.isEmpty()){ //Se edita el producto pero no se cambia la imagen
+
+            product.setImage(product.getImage());
+        }else{ //Se edita el producto y se edita la imagen
+            if (!product1.getImage().equals("default.jpg")){
+                uploadFileService.delete(product.getImage());
+            }
+            String imageName= uploadFileService.saveImage(file);
+            product.setImage(imageName);
+        }
+        product.setUser(product1.getUser());
         productService.update(product);
         return "redirect:/products";
     }
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable Long id){
+        Product product=new Product();
+        product=productService.get(id).get();
+        if (!product.getImage().equals("default.jpg")){
+            uploadFileService.delete(product.getImage());
+        }
+
         productService.delete(id);
         return "redirect:/products";
     }
